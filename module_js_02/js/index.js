8c367e89ef6bc0e6c09e84cf69a9eeadb2d82be9
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2018-11-11 12:41:43 +0200

    modified

--- module_js_02/js/index.js
+++ module_js_02/js/index.js
@@ -29,16 +29,16 @@ let total = 0;
 do {
   userInput = prompt('Введите число')
   
-  if(userInput === null) {
-    break
-  } else if (Number(userInput)) {
-    numbers.push(Number(userInput))
-  } else {
-    alert('Было введено не число, попробуйте еще раз!')
+  if (!Number.isNaN(+userInput) && userInput !== null) {
+    numbers.push(+userInput);
+  } else if (userInput !== null) {
+    alert('Не число');
   }
-} while (userInput)
+} while (userInput !== null);
 
-for(let i = 0; i < numbers.length; i += 1) {
-  total += numbers[i]
+if(numbers.length > 0) {
+  for(const value of numbers) {
+    total += value
+  }
+  alert(`Общая сумма чисел ${total}`);
 }
-alert(`Общая сумма чисел ${total}`);
